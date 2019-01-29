56ded3ff7f8c2d64007e85caefa80211247ee54c
Steve Albonico <salbonico@gmail.com>
2019-01-29 03:16:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "select authors.name, series.subgenre_id.name from series inner join authors on series.author_id = authors.id;"
+  "select authors.name, subgenres.name from series inner join authors on series.author_id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
