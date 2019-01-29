79a90b32be824dc60e928fd461816a2cbdca973e
Steve Albonico <salbonico@gmail.com>
2019-01-29 03:26:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select series.title from characters inner join series on characters.series_id = series.id group by (count(characters.species)) having characters.species = 'human'"
+  "select series.title from characters inner join series on characters.series_id = series.id group by (count(characters.species)) having characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
