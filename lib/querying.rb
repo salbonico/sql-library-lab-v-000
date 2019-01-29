ba8adae6ec904986b7b0fdd4138bb76c28ec0064
Steve Albonico <salbonico@gmail.com>
2019-01-29 02:55:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select books.title, books.year where books.series_id = 1 order by books.year ASC;"
+  "select books.title, books.year from books where books.series_id = 1 order by books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
