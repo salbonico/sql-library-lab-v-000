e908d42a2d59948a661914c2025ae779639fa6dd
Steve Albonico <salbonico@gmail.com>
2019-01-29 02:33:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 insert into series (title,author_id,subgenre_id) values ('lotr',1,1);
 insert into series (title,author_id,subgenre_id) values ('lotr',1,1);
+insert into books (title, year, series_id) values ('hello',1982,1);
