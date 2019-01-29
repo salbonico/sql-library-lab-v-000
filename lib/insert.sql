0df8c74c6b30f00f23ae801e9e0eb5d50789924b
Steve Albonico <salbonico@gmail.com>
2019-01-29 02:35:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,3 +6,4 @@ insert into books (title, year, series_id) values ('hello',1982,1);
 insert into books (title, year, series_id) values ('hello',1982,1);
 insert into books (title, year, series_id) values ('hello',1982,1);
 insert into books (title, year, series_id) values ('hello',1982,1);
+insert into characters (name, motto, species, author_id, series_id) values ('chance','woof','doggo',1,1)
